8955311157a55f542b602e1c4161c739286d817d
RazerP4antom <mirjisarthak3@gmail.com>
2023-04-19 16:28:06 +0530

    update

--- chrome extention/script.js
+++ chrome extention/script.js
@@ -1,4 +1,4 @@
-let myLeads = ["chutiya", "madarchod", "bosdi"];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 
@@ -6,8 +6,23 @@ const inputBtn = document.querySelector("#input-btn");
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
 
-for(let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" +  myLeads[i] + "</li>";
-}
+const renderLeads = () => {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    // ulEl.innerHTML += "<li>" +  myLeads[i] + "</li>";
+    // const li = document.createElement("li");
+    // li.textContent = myLeads[i];
+    // ulEl.append(li);
+    listItems +=
+      `<li>
+        <a target='_blank' href='${myLeads[i]}'> 
+         ${myLeads[i]} 
+        </a>
+      </li>`;
+  }
+  ulEl.innerHTML = listItems;
+};
