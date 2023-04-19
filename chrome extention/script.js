fb9230704dbccfde575e7cac9eb34a1945599f32
RazerP4antom <mirjisarthak3@gmail.com>
2023-04-19 21:28:32 +0530

    localstorage and manifest json

--- chrome extention/script.js
+++ chrome extention/script.js
@@ -1,28 +1,38 @@
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
+// localStorage.clear();
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 const inputBtn = document.querySelector("#input-btn");
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
   inputEl.value = "";
+  // myLeads = JSON.stringify(myLeads);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
+  // console.log(localStorage.getItem("myLeads"));
+  // console.log(getLeads());
 });
 
-const renderLeads = () => {
+
+function renderLeads(){
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     // ulEl.innerHTML += "<li>" +  myLeads[i] + "</li>";
     // const li = document.createElement("li");
     // li.textContent = myLeads[i];
     // ulEl.append(li);
-    listItems +=
-      `<li>
+    listItems += `<li>
         <a target='_blank' href='${myLeads[i]}'> 
          ${myLeads[i]} 
         </a>
       </li>`;
   }
   ulEl.innerHTML = listItems;
-};
+}
