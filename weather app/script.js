97b476f5b37015691b3fdbbc2fdcf9e42076bbdf
RazerP4antom <mirjisarthak3@gmail.com>
2023-04-21 13:56:53 +0530

    404 error handling

--- weather app/script.js
+++ weather app/script.js
@@ -3,6 +3,11 @@ let cityName = document.querySelector(".city");
 let temperature = document.querySelector(".temp");
 let humidity = document.querySelector(".humidity");
 let windSpeed = document.querySelector(".wind");
+const image = document.querySelector(".weather-icon");
+      // const cityName = document.querySelector(".city");
+      // const temperature = document.querySelector(".temp");
+      // const humidity = document.querySelector(".humidity");
+      // const windSpeed = document.querySelector(".wind");
 
 // async function chechWeather() {
 //   const response = await fetch(apiUrl);
@@ -18,18 +23,23 @@ const searchWeatherBtn = document.querySelector("#search-btn");
 searchWeatherBtn.addEventListener("click", () => {
   const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
   const inputCity = document.querySelector("#city-name").value;
+  if (inputCity === "") return;
 
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${inputCity}&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((json) => {
-      const image = document.querySelector(".weather-icon");
-      const cityName = document.querySelector(".city");
-      const temperature = document.querySelector(".temp");
-      const humidity = document.querySelector(".humidity");
-      const windSpeed = document.querySelector(".wind");
-      //   console.log(json);
+      if (json.cod === "404") {
+        image.src = "images/404.png";
+        cityName.innerHTML = "Error";
+        temperature.innerHTML = "";
+        humidity.innerHTML = "";
+        windSpeed.innerHTML = "";
+      }
+
+      
+      console.log(json);
       switch (json.weather[0].main) {
         case "Clear":
           image.src = "images/clear.png";
@@ -59,5 +69,3 @@ searchWeatherBtn.addEventListener("click", () => {
       windSpeed.innerHTML = `${parseInt(json.wind.speed)}km/h`;
     });
 });
-
-// chechWeather();
